a6857dfbe13925611a597921c73ce8c07a95f9a8
Felipe Lima <flf.felipe@gmail.com>
2016-05-03 20:22:44 -0300

    Commit inicial.

---
+++ javascripts/validadores.js
@@ -0,0 +1,22 @@
+﻿function esconderMsgs() {
+	var mensagens = document.getElementsByClassName("mensagem");
+	for (var i = 0; i < mensagens.length; i++) {
+		mensagens[i].innerHTML = "";
+		mensagens[i].style.visibility = "hidden";
+	}
+}
+
+function validarCampoVazio(idCampo) {
+	var campo = document.getElementById(idCampo);
+	var msg = document.getElementById("msg_" + idCampo);
+	
+	if (campo.value === "") {
+		msg.innerHTML = "Campo Inválido";
+		msg.style.visibility = "visible";
+		msg.style.color = "red";
+		msg.style.fontWeight = "bold";
+		msg.style.margin = "2px 0 0 0";
+	} else {
+		esconderMsgs();
+	}
+}
